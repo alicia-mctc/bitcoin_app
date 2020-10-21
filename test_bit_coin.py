a5aed1b74e7edf73b8ddd84ebe485926dd074299
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-20 23:40:47 -0700

    create bit coin file

--- test_bit_coin.py
+++ test_bit_coin.py
@@ -6,5 +6,10 @@ import bit_coin
 
 class TestBitCoin(Testcase):
 
-    def test_get_requests(self):
-        self.assertEqual()
+    @patch('bit_coin.get_requests')# test response for get requests()
+    def test_get_requests(self, mock_coins):
+        mock_coins = 30
+        api_response = {"USD":{"code":"USD","symbol":"&#36;","rate":"12,024.9550","description":"United States Dollar","rate_float":12024.955}}
+        mock_coins.side_effect = [api_response]
+        expected = 20
+        self.assertEqual(expected)
