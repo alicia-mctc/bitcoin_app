46310c10d2ed83397ba53387bd49e44fcfefde4d
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-20 22:16:29 -0700

    add get function()

--- bit_coin.py
+++ bit_coin.py
@@ -3,19 +3,20 @@ from pprint import pprint
 
 try:
     coindesk_url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    
+    def get_requests():
+        response = requests.get(coindesk_url)   
+        data = response.json()
+        #pprint(data)
 
-    response = requests.get(coindesk_url)   
-    data = response.json()
-    #pprint(data)
+        dollars_exchange_rate = data['bpi']['USD']['rate_float']
+        print(dollars_exchange_rate)
 
-    dollars_exchange_rate = data['bpi']['USD']['rate_float']
-    print(dollars_exchange_rate)
+        bitcoin = float(input('Enter the number of bitcoin: '))
 
-    bitcoin = float(input('Enter the number of bitcoin: '))
+        bitcoin_value_in_dollars = bitcoin * dollars_exchange_rate
 
-    bitcoin_value_in_dollars = bitcoin * dollars_exchange_rate
-
-    print(f'{bitcoin} Bitcoin is equivalent to $ {bitcoin_value_in_dollars}')
+        print(f'{bitcoin} Bitcoin is equivalent to $ {bitcoin_value_in_dollars}')
 
 except Exception as e:
     print(e)
---
+++ test_bit_coin.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import patch
+
+import bit_coin
+
+class TestBitCoin(Testcase):
+
+    def test_get_requests(self):
+        self.assertEqual()
